79ad65ab5f932807b1df425c43c1f562f5a1314d
TheGift <djadunov@gmail.com>
2026-01-23 14:06:14 +0100

    bigger elements for mobile v2

--- script.js
+++ script.js
@@ -38,8 +38,8 @@ function loadMarathonsFromURL(url, callback) {
 
 
 // Detect mobile devices for further optimizations
-const isMobile = /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-const isSmallScreen = window.innerWidth < 768;
+const isMobile = /Mobi|Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|webOS/i.test(navigator.userAgent);
+const isSmallScreen = window.innerWidth < 1024; // Increased threshold
 const isVerySmallScreen = window.innerWidth < 480;
 
 const viewer = new Cesium.Viewer('cesiumContainer', {
@@ -164,7 +164,7 @@ closeBtn.addEventListener('click', hideOverlay);
 
 loadMarathonsFromURL("marathons.txt", function(marathonPoints){
 
-const arrowSizeNum = isSmallScreen ? 80 : 40;
+const arrowSizeNum = isSmallScreen ? 100 : 40;
 
     const points = marathonPoints.map(p => {
         const entity = viewer.entities.add({
@@ -394,7 +394,7 @@ const arrowSizeNum = isSmallScreen ? 80 : 40;
         // Calculate star size based on screen width (larger on smaller screens)
         let starSize;
         if (isSmallScreen) {
-            starSize = Math.max(80, screenWidth / 3);
+            starSize = Math.max(100, screenWidth / 2);
         } else {
             starSize = 32;
         }
@@ -407,12 +407,12 @@ const arrowSizeNum = isSmallScreen ? 80 : 40;
         const label = document.getElementById('uiLabel');
         if (isVerySmallScreen) {
             label.style.width = '95%'; // Even larger for very small phones
-            label.style.fontSize = '60px';
-            label.style.padding = '60px 65px';
+            label.style.fontSize = '72px';
+            label.style.padding = '70px 75px';
         } else if (isSmallScreen) {
             label.style.width = '90%';
-            label.style.fontSize = '48px';
-            label.style.padding = '50px 55px';
+            label.style.fontSize = '60px';
+            label.style.padding = '60px 65px';
         } else {
             label.style.width = 'auto';
             label.style.minWidth = '229px';
